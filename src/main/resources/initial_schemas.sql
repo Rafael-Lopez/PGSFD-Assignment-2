5330a05164befafdafb446c14ca2b9b97f0c5f99
Rafael Lopez <rafael.lopez@bbd.ca>
2021-01-25 21:32:15 -0500

    #4: As a Developer, I would like to add a system user DB table

--- src/main/resources/initial_schemas.sql
+++ src/main/resources/initial_schemas.sql
@@ -57,5 +57,14 @@ ENGINE=InnoDB
 DEFAULT CHARSET=utf8mb4
 COLLATE=utf8mb4_0900_ai_ci;
 
+CREATE TABLE academy.system_user (
+	id bigint NOT NULL AUTO_INCREMENT,
+	username varchar(45) NOT NULL,
+	password varchar(45) NOT NULL,
+	PRIMARY KEY (id)
+)
+ENGINE=InnoDB
+DEFAULT CHARSET=utf8mb4
+COLLATE=utf8mb4_0900_ai_ci;
 
 
